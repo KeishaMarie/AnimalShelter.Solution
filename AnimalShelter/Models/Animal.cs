e379212e2cd6bbb27a722687d2f7dedcf7c2afb8
Keisha Hepner <kmhepner90@gmail.com>
2022-10-28 16:49:55 -0700

    Update controller and models, modified startup code

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public int Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+    public bool Fixed { get; set; }
+  }
+}
